6acbbc593f771a094c79729c86a8a65c37d9a8fa
Mohssine CHKIRIDA <mhsnchkirida@gmail.com>
2026-05-08 00:02:19 +0100

    Delete Climb-Guide directory

--- Climb-Guide/database.js
+++
@@ -1,9 +0,0 @@
-const Pool = require('pg').Pool; 
-const pool = new Pool({ 
-    user: "postgres", 
-    password: "root", 
-    database: "Refuge", 
-    host: "localhost", 
-    port: "5432" 
-}); 
-module.exports = pool; 
--- Climb-Guide/page-de-refuges/server/data/equipment.js
+++
@@ -1,7 +0,0 @@
-export const equipment = [
-  { name: 'Cordes', available: true },
-  { name: 'Crampons', available: true },
-  { name: 'Piolets', available: true },
-  { name: 'Tentes', available: true },
-  { name: 'Raquettes à neige', available: true }
-];
